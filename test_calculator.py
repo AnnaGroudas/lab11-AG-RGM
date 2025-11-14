374b3eb595b0afe2f58dfe9d40941952231e1720
riya george mathew <riyageorgemathew@riyas-MacBook-Pro.local>
2025-11-14 14:57:12 -0500

    partner 2 test_calculator

---
+++ test_calculator.py
@@ -0,0 +1,67 @@
+import unittest
+from calculator import *
+
+class TestCalculator(unittest.TestCase):
+    ######### Partner 2
+    # def test_add(self): # 3 assertions
+    #     fill in code
+
+    # def test_subtract(self): # 3 assertions
+    #     fill in code
+    # ##########################
+
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(5,4), 20)
+        self.assertEqual(mul(0,0),0)
+        self.assertEqual(mul(-12,-4),48)
+        self.assertEqual(mul(-3,20),-60)
+
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(4,2),0.5)
+        self.assertEqual(div(-3,9),-3)
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 0)
+
+    ######## Partner 2
+    # def test_divide_by_zero(self): # 1 assertion
+    #     # call division function inside, example:
+    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     #     div(0, 5)
+    #     fill in code
+
+    # def test_logarithm(self): # 3 assertions
+    #     fill in code
+
+    # def test_log_invalid_base(self): # 1 assertion
+    #     # use same technique from test_divide_by_zero
+    #     fill in code
+    # ##########################
+
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
+        with self.assertRaises(ValueError):
+            log(0,5)
+            log(-4,10)
+
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3,4),5)
+        self.assertEqual(hypotenuse(8,6),10)
+
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        with self.assertRaises(ValueError):
+            square_root(-2)
+            square_root(-100)
+        self.assertEqual(square_root(100),10)
+        self.assertEqual(square_root(36),6)
+        self.assertEqual(square_root(1),1)
+        self.assertEqual(square_root(0),0)
+
+# Do not touch this
+if __name__ == "__main__":
+    unittest.main()
