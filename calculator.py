d5856531b4056187579cad00c3e56b25cae3949d
Anna Groudas <anna.groudas@gmail.com>
2025-11-14 15:03:39 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,6 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-<<<<<<< HEAD
-# First example
-# def add(a, b):
-#     pass
-
-import math
-=======
+# https://github.com/AnnaGroudas/lab11-AG-RGM
+# Partner 1: Anna Groudas
+# Partner 2: Riya George Mathew
 
 import math
 
@@ -22,7 +12,6 @@ def square_root(a):
 def hypotenuse(a, b):
     return math.hypot(a, b) # can have negative nums
 
->>>>>>> a6ed769f805cb3aa723e96d10a548336f9744e4f
 def add(a, b):
     return a + b
 
@@ -33,30 +22,15 @@ def mul(a, b):
     return a * b
 
 def div(a, b):
-<<<<<<< HEAD
     if a==0:
         raise ZeroDivisionError("Cannot divide by zero!")
     return b / a # raise ZeroDivisionError if a == 0
-=======
-    if a ==0:
-        raise ZeroDivisionError("Cannot divide by zero!")
-    return b/a
->>>>>>> a6ed769f805cb3aa723e96d10a548336f9744e4f
 
 def log(a, b):
     if a<=0:
         raise ValueError("a must be a positive value!")
     return math.log(b,a) # use math library + raise ValueError
 
-<<<<<<< HEAD
-
 def exp(a, b):
     return a**b
-=======
-def exp(a, b):
-    return a**b
-
-
->>>>>>> a6ed769f805cb3aa723e96d10a548336f9744e4f
-
-
+    
